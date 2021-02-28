a41a770c6dde9c14618bd833ea6d3fc45c3086fb
chaoslight1023 <79743483+chaoslight1023@users.noreply.github.com>
2021-02-28 11:44:42 +0800

    Delete sign in.py

--- sign in.py
+++
@@ -1,9 +0,0 @@
-for i in range (1,6):
-    username=input('请输入姓名：')
-    if username==('戴严'):
-        print('欢迎登入')
-        break
-    else:
-        print('你还有%d次机会'%(5-i))
-else:
-    print('失败次数过多，无法登入')
